6d2cf0d6d7fd8d0c2a4c4befb858c3c5a8a2e8e6
PashaBurgaz <bthnbin963@icloud.com>
2024-07-14 20:44:04 +0300

    Create dclink.js

---
+++ komutlar/dclink.js
@@ -0,0 +1,25 @@
+const {EmbedBuilder} = require("discord.js");
+
+exports.run = async (client, message, args) => {
+
+    const embed = new EmbedBuilder()
+    .setTitle("Sunucu Davet")
+    .setDescription(`**[KATILMAK İÇİN TIKLA](https://discord.gg/zZ4BY8uVZ4)
+    https://discord.gg/zZ4BY8uVZ4 **\n`)
+    .setColor("#ff0000")
+    .setTimestamp()
+    return message.channel.send({embeds : [embed]});
+
+};
+
+module.exports.conf = {
+aliases: ['dc','link'],
+permLevel: 0, 
+kategori: 'Genel'
+};
+
+module.exports.help = {
+    name: 'dc',
+    description: 'Discord linki atar.',
+    usage: 'dc'
+};
