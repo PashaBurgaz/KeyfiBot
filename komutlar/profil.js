141147a0067500611e7b79559e9cffcc3f776356
PashaBurgaz <bthnbin963@icloud.com>
2024-07-14 20:45:02 +0300

    Create profil.js

---
+++ komutlar/profil.js
@@ -0,0 +1,32 @@
+const Discord  = require('discord.js');
+exports.run = async (bot, message, args) => {
+
+let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member
+
+const embed = new Discord.EmbedBuilder()
+.setColor("#ff0000")
+.setTimestamp()
+.setTitle(user.user.tag)
+.setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
+.setDescription(`
+> **Discord'a katılma tarihi:** <t:${Math.floor(user.user.createdTimestamp / 1000)}:d>
+> **Sunucuya katılma tarihi:** <t:${Math.floor(user.joinedTimestamp / 1000)}:d>
+> **Sunucuda ki adı:** \`${user.displayName}\`
+> **Durum:** \`${user.presence?.status ? user.presence?.status.replace("dnd", "Rahatsız Etmeyin").replace("idle", "Boşta").replace("online", "Aktif").replace("offline", "Aktif Değil") : "Aktif Değil"}\`
+> **ID:** \`${user.id}\``)
+
+return message.reply({ embeds : [embed] })
+}
+
+exports.conf = {
+    aliases: ['profil'], 
+    permLevel: 0,
+    kategori: "Genel" 
+  };
+
+  exports.help = {
+    name: 'profil',  
+    description: 'Profil bilgisi verir.', 
+    usage: 'profil', 
+  };
+
